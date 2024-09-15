0fe001c7e812efe99124666e721a80f60415acaf
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-09-14 20:57:36 -0700

    tailwind colors, other stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,25 +2,33 @@ const Home = () => {
 
   return (
     <>
-      <div id='top-bar'>
+      <div id='top-bar' class="flex mt-5">
+        <div class="w-[33%] items-center flex"></div>
+        <div class='w-[33%] flex justify-center'>
+          <h1 class='text-xl'>Savvy Saver</h1>
+        </div>
         {/* TODO: connect backend */}
-        <h3>Points: 123123</h3>
-      </div>
-
-      <div id='challenge-progress'>
-        <h2>Challenge progress</h2>
-        {/* TODO: connect to backend */}
-        <progress value={0.5} />
-        <div>
-          {/* TODO: connect these to backend */}
-          <p>Day 1 of 7</p>
-          <p>50$ out of 100$ left!</p>
+        <div class="w-[33%] flex justify-end mr-5 items-center">
+          <h3 class="">Points: 123123</h3>
         </div>
       </div>
 
-      <div id='in-depth-report'>
-        <h2>In depth report</h2>
-        <p>
+      <div id='challenge-progress' class="flex justify-center text-lightyellow">
+        <div class="w-[80%] flex flex-col justify-center text-center mt-10">
+          <h2>Challenge progress</h2>
+          {/* TODO: connect to backend */}
+          <progress value={0.5} />
+          <div>
+            {/* TODO: connect these to backend */}
+            <p>Day 1 of 7</p>
+            <p>50$ out of 100$ left!</p>
+          </div>
+        </div>
+      </div>
+
+      <div id='in-depth-report' class='m-20'>
+        <h2 class='mb-5 text-lg'>In depth report</h2>
+        <p class="text-sm">
           {/* TODO: Integrate ChatGPT equivalent */}
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam justo eros, vestibulum id sem in, mattis congue elit. Vivamus lacus velit, tristique sed sodales non, tristique non ante. Nulla interdum mauris sit amet tempor molestie. Cras nec mi non quam tincidunt rutrum. Nulla aliquet suscipit nulla. Vestibulum commodo consequat nulla. Quisque pharetra elementum neque sit amet suscipit. Phasellus tincidunt feugiat commodo. Sed sodales finibus vulputate. Nunc non odio porta, commodo tortor ut, iaculis est. Nulla euismod velit facilisis, ornare urna vel, tempor dolor. Curabitur non tortor eu risus pellentesque condimentum in mattis turpis. Phasellus eget lacinia massa.
         </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontSize: {
+      sm: '22px',
+      base: '28px',
+      lg: '34px',
+      xl: '40px',
+    },
+    colors: {
+      black: '#000000',
+      red: '#ff0000',
+      yellow: '#ffff00',
+      white: '#ffffff',
+      lightyellow: '#fdf6c4',
+    },
     extend: {},
   },
   plugins: [],
 }
 
+// black: #000000
+// red: #c45039
+// gold: e4ba29
