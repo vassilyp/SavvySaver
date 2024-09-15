8de1b60b3ea44e7cf26b6fb64b8f6abdc09864da
Vassily-Petrousevitch <vasspetrousevitch@gmail.com>
2024-09-15 04:40:04 -0700

    more styles for pages

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,7 +1,11 @@
 const SubmitButton = ({ onClick }) => { // Destructure props to get onClick
     return (
-      <button onClick={onClick}>Submit</button>
-    );
-  };
-  
-  export default SubmitButton;
+    <div className="flex w-full justify-center">
+      <button className="mr-12 flex bg-accentDark text-center p-2 px-8 rounded-xl" onClick={onClick}>
+        <p className="text-white">Submit</p>
+      </button>
+    </div>
+  );
+};
+
+export default SubmitButton;
